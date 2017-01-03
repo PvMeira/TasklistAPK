e9d43324db56e26b4c228b81d18f5a72af5b2565
Pedro Vitor <Pedro Vitor>
2017-01-02 23:26:03 -0200

    Add second Activity

--- app/src/main/java/com/br/pvciria/tasklist/MainActivity.java
+++ app/src/main/java/com/br/pvciria/tasklist/MainActivity.java
@@ -1,7 +1,9 @@
 package com.br.pvciria.tasklist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onSecondActivity(View view){
+        Intent it = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(it);
+    }
+
 }
